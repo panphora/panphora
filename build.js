632c0b9e84fa1f67e926c7324e171234c70b7a50
David Miranda <david@storylog.com>
2023-11-09 14:48:26 -0500

    better link

---
+++ build.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+const directoryPath = './';
+
+fs.readdir(directoryPath, (err, files) => {
+  if (err) {
+    console.error('Could not read directory:', err);
+    return;
+  }
+
+  files.forEach(file => {
+    if (path.extname(file) === '.html') {
+      const filePath = path.join(directoryPath, file);
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          console.error('Could not read file:', err);
+          return;
+        }
+        
+        // Replace both '../panphora-js/' and '../panphora-css/' with production URLs
+        let updatedData = data.replace(/\.\.\/panphora-js\//g, 'https://js.panphora.com/');
+        updatedData = updatedData.replace(/\.\.\/panphora-css\//g, 'https://css.panphora.com/');
+        
+        fs.writeFile(filePath, updatedData, 'utf8', err => {
+          if (err) {
+            console.error('Could not write to file:', err);
+          }
+        });
+      });
+    }
+  });
+});
